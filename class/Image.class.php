9dcb92dfa55a4498f1a55be1d5fb9e8837cc2a84
Raju Rayhan <raju.rayhan@yandex.com>
2022-12-14 15:20:15 +0600

    Added git ignore and structured the app

--- class/Image.class.php
+++ class/Image.class.php
@@ -16,41 +16,45 @@
 
 		
 		public function grabImage($image_url, $image_file, $file_name, $folder){
-		    $fp = fopen ($image_file, 'w+');              // open file handle
+			$path 		= 'storage/downloads/'.$folder;
 
-		    $ch = curl_init($image_url);
-		    // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // enable if you want
-		    curl_setopt($ch, CURLOPT_FILE, $fp);          // output to file
-		    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-		    curl_setopt($ch, CURLOPT_TIMEOUT, 1000);      // some large value to allow curl to run for a long time
-		    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 || Raju Image Downlaod');
-		    // curl_setopt($ch, CURLOPT_VERBOSE, true);   // Enable this line to see debug prints
-		    curl_exec($ch);
-
-		    curl_close($ch);                              // closing curl handle
-		    fclose($fp);                                  // closing file handle	
-
-		    $name = $this->addWaterMark($image_file,  $file_name, $folder);
-
-		    return $name;	
-		}
-
-		public function addWaterMark($imageFile,  $file_name, $folder)
-		{
-			$watermark 	= Image::make('storage/new_institute_logo.png'); 
-
-			$img            = Image::make($imageFile);
-            $img->insert($watermark, 'bottom-right',10,10);
-			$path = 'storage/downloads/'.$folder;
 			if (!file_exists($path)) {
 				mkdir($path, 0777, true);
 			}
-            $img->save($path.'/'.$file_name);
 
+			$filePath 	= $path.'/'.$file_name;
+		    if(!file_exists($filePath)){
+		    	$fp = fopen ($image_file, 'w+');              // open file handle
+
+			    $ch = curl_init($image_url);
+			    // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // enable if you want
+			    curl_setopt($ch, CURLOPT_FILE, $fp);          // output to file
+			    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			    curl_setopt($ch, CURLOPT_TIMEOUT, 1000);      // some large value to allow curl to run for a long time
+			    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 || Raju Image Downlaod');
+			    // curl_setopt($ch, CURLOPT_VERBOSE, true);   // Enable this line to see debug prints
+			    curl_exec($ch);
+
+			    curl_close($ch);                              // closing curl handle
+			    fclose($fp);                                  // closing file handle	
+
+			    $name = $this->addWaterMark($image_file, $filePath);
+			    return $name;
+		    }
+
+		    return $filePath;
+		}
+
+		public function addWaterMark($imageFile,  $filePath)
+		{
+			$watermark 	= Image::make('watermarks/new_institute_logo.png'); 
+
+			$img = Image::make($imageFile);
+            $img->insert($watermark, 'bottom-right',10,10); 
+            $img->save($filePath);
             unlink($imageFile);
 
-			// echo "<img src='storage/water/".$name."'>";
-			return $file_name;
+			return $filePath;
 		}
 	}
 ?>
